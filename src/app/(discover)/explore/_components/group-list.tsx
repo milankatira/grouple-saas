deea87f160120434cbfd000aa91920c28d613877
milankatira26 <milan@instaservice.com>
2024-11-05 18:45:10 +0530

    feat: :fire: complete auth flow

--- src/app/(discover)/explore/_components/group-list.tsx
+++ src/app/(discover)/explore/_components/group-list.tsx
@@ -5,30 +5,30 @@ import GroupCard from "./group-card"
 import PaginatedGroups from "./paginated-groups"
 
 type Props = {
-  category: string
+    category: string
 }
 
 const GroupList = ({ category }: Props) => {
-  const { groups, status } = useGroupList("groups")
+    const { groups, status } = useGroupList("groups")
 
-  return (
-    <div className="container grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-6 mt-16">
-      {status === 200 ? (
-        groups.map((group) => <GroupCard key={group.id} {...group} />)
-      ) : (
-        <NoResult />
-      )}
-      {groups && groups.length > 5 && (
-        <InfiniteScrollObserver
-          action="GROUPS"
-          identifier={category}
-          paginate={groups.length}
-        >
-          <PaginatedGroups />
-        </InfiniteScrollObserver>
-      )}
-    </div>
-  )
+    return (
+        <div className="container grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-6 mt-16">
+            {status === 200 ? (
+                groups.map((group) => <GroupCard key={group.id} {...group} />)
+            ) : (
+                <NoResult />
+            )}
+            {groups && groups.length > 5 && (
+                <InfiniteScrollObserver
+                    action="GROUPS"
+                    identifier={category}
+                    paginate={groups.length}
+                >
+                    <PaginatedGroups />
+                </InfiniteScrollObserver>
+            )}
+        </div>
+    )
 }
 
 export default GroupList
--- src/app/group/[groupid]/channel/[channelid]/[postid]/_components/post-info/index.tsx
+++ src/app/group/[groupid]/channel/[channelid]/[postid]/_components/post-info/index.tsx
@@ -8,47 +8,47 @@ import { Interactions } from "../../../_components/post-feed/interactions"
 import { PostAuthor } from "../../../_components/post-feed/post-author"
 
 type PostInfoProps = {
-  id: string
+    id: string
 }
 
 export const PostInfo = ({ id }: PostInfoProps) => {
-  const { data } = useGetPost(id)
+    const { data } = useGetPost(id)
 
-  if (data?.status !== 200 || !data)
+    if (data?.status !== 200 || !data)
+        return (
+            <div>
+                <NoResult />
+            </div>
+        )
+
     return (
-      <div>
-        <NoResult />
-      </div>
+        <div className="flex flex-col gap-y-5">
+            <PostAuthor
+                username={`${data?.post?.author.firstname} ${data?.post?.author.lastname}`}
+                image={data.post?.author.image as string}
+                channel={data.post?.channel.name as string}
+            />
+            <div className="flex flex-col gap-y-3">
+                <h2 className="text-2xl font-bold">{data.post?.title}</h2>
+                <HtmlParser html={data.post?.htmlContent as string} />
+            </div>
+            <Interactions
+                id={id}
+                page
+                userid={data.post?.authorId}
+                likedUser={
+                    data.post && data.post?.likes.length > 0
+                        ? data.post.likes[0].userId
+                        : undefined
+                }
+                likeid={
+                    data.post && data.post?.likes.length > 0
+                        ? data.post.likes[0].id
+                        : undefined
+                }
+                likes={data.post?._count.likes!}
+                comments={data.post?._count.comments!}
+            />
+        </div>
     )
-
-  return (
-    <div className="flex flex-col gap-y-5">
-      <PostAuthor
-        username={`${data?.post?.author.firstname} ${data?.post?.author.lastname}`}
-        image={data.post?.author.image as string}
-        channel={data.post?.channel.name as string}
-      />
-      <div className="flex flex-col gap-y-3">
-        <h2 className="text-2xl font-bold">{data.post?.title}</h2>
-        <HtmlParser html={data.post?.htmlContent as string} />
-      </div>
-      <Interactions
-        id={id}
-        page
-        userid={data.post?.authorId}
-        likedUser={
-          data.post && data.post?.likes.length > 0
-            ? data.post.likes[0].userId
-            : undefined
-        }
-        likeid={
-          data.post && data.post?.likes.length > 0
-            ? data.post.likes[0].id
-            : undefined
-        }
-        likes={data.post?._count.likes!}
-        comments={data.post?._count.comments!}
-      />
-    </div>
-  )
 }
